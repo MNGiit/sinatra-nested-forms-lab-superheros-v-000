ef94d462b39a738af43e1e5df1e6c8531e2ae645
Mohammed Noori <kmanoorib@gmail.com>
2018-12-28 06:15:01 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,14 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
-    set :views, Proc.new { File.join(root, "../views/") }
+  set :views, Proc.new { File.join(root, "../views/") }
 
-
+  get "/" do
+    erb :"../views/super_hero"
+  end
+  
+  get "/teams" do
+    erb :"../views/team"
+  end
+  
 end
